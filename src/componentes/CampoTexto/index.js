52999c31287d147f132bf57e2c5db1a087d77891
Flávio Pimentel <flawin@icloud.com>
2024-05-12 19:20:28 -0300

    first commit

---
+++ src/componentes/CampoTexto/index.js
@@ -0,0 +1,12 @@
+import './CampoTexto.css';
+
+const CampoTexto = () => {
+    return (
+        <div className="campoTexto">
+            <label>Nome</label>
+            <input placeholder='Digite seu nome'/>
+        </div>
+    )
+}
+
+export default CampoTexto
